85ac39cf7b02cad95d2c2a4daa25483855f10534
Anthony Castronuovo <ajc320@scarletmail.rutgers.edu>
2015-10-15 16:07:14 -0400

    moving obstacle script

--- BAssignments/B1/NavigationandAnimation/Assets/Scripts/CubesMoves.cs
+++ BAssignments/B1/NavigationandAnimation/Assets/Scripts/CubesMoves.cs
@@ -3,13 +3,27 @@ using System.Collections;
 
 public class CubesMoves : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public Vector3 pointB;
+
+    IEnumerator Start()
+    {
+        var pointA = transform.position;
+        while (true)
+        {
+            yield return StartCoroutine(MoveObject(transform, pointA, pointB, 3.0f));
+            yield return StartCoroutine(MoveObject(transform, pointB, pointA, 3.0f));
+        }
+    }
+
+    IEnumerator MoveObject(Transform thisTransform, Vector3 startPos, Vector3 endPos, float time)
+    {
+        var i = 0.0f;
+        var rate = 3.0f / time;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            thisTransform.position = Vector3.Lerp(startPos, endPos, i);
+            yield return null;
+        }
+    }
 }
