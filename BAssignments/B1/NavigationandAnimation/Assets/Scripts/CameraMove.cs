d61da06450186056e0eb4384f20847ccbfd813c0
Anthony Castronuovo <ajc320@scarletmail.rutgers.edu>
2015-10-16 22:11:09 -0400

    camera change
    camera rotates and can see everything in the level

---
+++ BAssignments/B1/NavigationandAnimation/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMove : MonoBehaviour {
+
+    public GameObject target = null;
+    public bool orbit = false;
+    private Vector3 offset = Vector3.zero;
+
+    public void Start()
+    {
+        offset = Input.mousePosition;
+    }
+
+     public void Update () {
+        if(target != null)
+        {
+            transform.LookAt(target.transform);
+            if(orbit)
+            {
+                transform.RotateAround(target.transform.position, Vector3.up, Time.deltaTime);
+            }
+        }
+
+    
+     }
+
+ }
