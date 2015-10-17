90a54da5817b181168935e1f676b8952e50263b5
Jon Lee <jonlee@nbp-134-128.nbp.ruw.rutgers.edu>
2015-10-17 10:22:22 -0400

    added human rig

---
+++ BAssignments/B1/NavigationandAnimation/Assets/Scripts/HumanScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HumanScript : MonoBehaviour {
+
+	private Animator anim;
+	private float h;
+	private float v;
+	private float j;
+	
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		h = Input.GetAxis ("Horizontal");
+		v = Input.GetAxis ("Vertical");
+		j = Input.GetAxis ("Jump");
+		
+		anim.SetFloat ("Walk", v);
+		anim.SetFloat ("Turn", h);
+		anim.SetFloat ("Jump", j);
+	}
+}
