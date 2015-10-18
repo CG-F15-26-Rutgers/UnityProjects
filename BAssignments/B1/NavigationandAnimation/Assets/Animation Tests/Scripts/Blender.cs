623e9f9673b0ab8c65941097ee7c02df6e3d8e9b
Jon Lee <jonlee@nbp-134-128.nbp.ruw.rutgers.edu>
2015-10-18 01:55:37 -0400

    walks

---
+++ BAssignments/B1/NavigationandAnimation/Assets/Animation Tests/Scripts/Blender.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blender : MonoBehaviour {
+	Animator anim;
+
+	void Start() {
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update() {
+		float h = Input.GetAxis ("Horizontal");
+		float v = Input.GetAxis ("Vertical");
+	
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Vector3 temp = transform.position;
+			temp.y += 3f;
+			transform.position = temp;
+		}
+
+		anim.SetFloat ("MoveSpeed", v);
+		anim.SetFloat ("TurnDirection", h);
+	}
+
+}
--- BAssignments/B1/NavigationandAnimation/Assets/Scripts/Director.cs
+++ BAssignments/B1/NavigationandAnimation/Assets/Scripts/Director.cs
@@ -2,7 +2,13 @@
 using System.Collections;
 
 public class Director : MonoBehaviour {
-	
+
+	bool humanAgent;
+
+	void Start() {
+		humanAgent = false;
+	}
+
 	void Update () {
 	    
 		if (Input.GetMouseButtonDown(0)){
@@ -11,18 +17,28 @@ public class Director : MonoBehaviour {
 
 			if (Physics.Raycast(ray.origin, ray.direction, out hit)){
 
+				if(hit.transform.gameObject.CompareTag("HumanAgent")) {
+					humanAgent = true;
+					hit.transform.gameObject.GetComponent<HumanAgentScript>().ChangeState();
+				}
+
 				// If the user clicked on an agent, then activate or deactivate the agent.
-				if (hit.transform.gameObject.CompareTag("Agent"))
+				 else if (hit.transform.gameObject.CompareTag("Agent")) {
+					humanAgent = false;
 					hit.transform.gameObject.GetComponent<AgentScript>().ChangeState();
-
+				}
 				// If the user clicked on an obstacle, then activate or deactivate the obstacle.
-				else if (hit.transform.gameObject.CompareTag("Obstacle"))
+				 else if (hit.transform.gameObject.CompareTag("Obstacle")) {
 					hit.transform.gameObject.GetComponent<ObjectScript>().ChangeState();
-
+				}
 				// Otherwise, change the agents' goals to the position the user clicked.
-				else
-					AgentScript.target = hit.point;
-			}
+				else {
+					if(humanAgent == true)
+						HumanAgentScript.target = hit.point;
+					else
+						AgentScript.target = hit.point;
+				}	
+			}	
 		}
 	}
 }
