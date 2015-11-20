264e5db4a4a4ab2e741e33b2f3262799d32c3173
Anthony Castronuovo <ajc320@scarletmail.rutgers.edu>
2015-11-20 18:32:02 -0500

    added sequence of actions
    sequence parallel(HeadLook,
    GoTo)
    Hand Wave

---
+++ BAssignments/B2/B2 Behavior Trees/Assets/KADAPT/Assets/KADAPT/B2 sCRIPTS/BG_goTo.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using TreeSharpPlus;
+
+public class BG_goTo : MonoBehaviour {
+
+    public Transform moveTo1;
+    public GameObject person;
+
+    private BehaviorAgent theGuy;
+
+	void Start () {
+        theGuy = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(theGuy);
+        theGuy.StartBehavior();
+	}
+	
+	void Update () {
+	
+	}
+
+    protected Node WalkToPos(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(person.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(2));
+    }
+
+    protected Node BuildTreeRoot()
+    {
+        return new DecoratorLoop(new Sequence(this.WalkToPos(this.moveTo1)));
+
+    }
+
+}
---
+++ BAssignments/B2/B2 Behavior Trees/Assets/KADAPT/Assets/KADAPT/B2 sCRIPTS/MainGuys.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using TreeSharpPlus;
+
+public class MainGuys : MonoBehaviour
+{
+
+    public Transform moveTo1;
+    public GameObject person;
+    public Transform orientTowardsThis;
+
+    private BehaviorAgent theGuy;
+
+    void Start()
+    {
+        theGuy = new BehaviorAgent(this.BuildTreeRoot());
+        BehaviorManager.Instance.Register(theGuy);
+        theGuy.StartBehavior();
+    }
+
+    void Update()
+    {
+
+    }
+
+    protected Node WalkToPos(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(person.GetComponent<BehaviorMecanim>().Node_GoTo(position));
+    }
+
+    protected Node LookAtThing(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(person.GetComponent<BehaviorMecanim>().Node_OrientTowards(position));
+    }
+
+    protected Node HeadLookAtThing(Transform target)
+    {
+        Val<Vector3> position = Val.V(() => target.position);
+        return new Sequence(person.GetComponent<BehaviorMecanim>().Node_HeadLook(position));
+    }
+
+    protected Node handWave()
+    {
+        return new Sequence(person.GetComponent<BehaviorMecanim>().Node_HandAnimation("Wave", true));
+    }
+
+    protected Node BuildTreeRoot()
+    {
+       // Debug.Log("Sequence" + );
+        return new Sequence( LookAtThing(orientTowardsThis), new SequenceParallel(HeadLookAtThing(orientTowardsThis), WalkToPos(moveTo1)) ,  handWave());
+
+    }
+
+}
